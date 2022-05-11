956380767985a53dbcfe4ac3cc58efde80e7c021
dcb99 <31573956+dcb99@users.noreply.github.com>
2022-05-11 13:49:39 -0400

    Initial commit - coder dotfiles

---
+++ config.fish
@@ -0,0 +1,7 @@
+alias pull "git pull"
+alias push "git push"
+alias lear "clear"
+alias clea "clear"
+alias r "clear"
+
+neofetch | lolcat
---
+++ install.sh
@@ -0,0 +1,5 @@
+echo "Copy config.fish to .config/fish/"
+cp config.fish /home/coder/.config/fish/
+
+echo "Install wanted packages"
+sudo apt-get install -y neofetch lolcat
